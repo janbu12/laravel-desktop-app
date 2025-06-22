cf42e2ecece6571466d94eb0444baf638ff80d79
janbu12 <mizanmujadid2436@gmail.com>
2025-06-23 01:27:16 +0700

    first commit

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\Window;
+use Native\Laravel\Contracts\ProvidesPhpIni;
+use Native\Laravel\Facades\MenuBar;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open()
+            ->maximized();
+        MenuBar::create();
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <div class="w-full h-svh overflow-y-auto p-4">
+        <h1 class="text-2xl font-semibold">This is Home</h1>
+    </div>
+</x-layout>
